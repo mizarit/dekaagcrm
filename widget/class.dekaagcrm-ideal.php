37eef66fc2a42a423d6beda8c9d2d7e64999cba3
Ricardo <ricardo@dev.mizar-it.nl>
2014-12-15 17:31:25 +0000

    Added debug info for iDeal problem

--- widget/class.dekaagcrm-ideal.php
+++ widget/class.dekaagcrm-ideal.php
@@ -28,105 +28,121 @@ class DeKaagCRM_Ideal extends WP_Widget {
 	public function widget($args, $instance)
 	{
 	  echo $args['before_widget'];
-	  
+
+	  if (in_array($_SERVER['REMOTE_ADDR'], array('94.209.9.24', '192.168.8.1'))) {
+	    var_dump($_GET);
+	    var_dump($instance);
+	    var_dump($_SERVER);
+	    var_dump($_REQUEST);
+	    var_dump($_SESSION);
+	  }
 	  if (!empty($instance['mollie_key']) && isset($_GET['invoice'])) {
 	    
 	    $invoice = DeKaagInvoice::model()->findByCode($_GET['invoice']);
-	    $hash = crypt($invoice->invoicenr, $invoice->invoicenr.'DEKAAG');
 	    
-	    if (isset($_GET['hash']) && $_GET['hash'] == $hash) {
-  	    $total = $invoice->getTotalRemainingStr(false);
-  	    $downpayment = false;
-  	    if (isset($_GET['downpayment']) && $invoice->downpayment != 'none') {
-  	      if ($invoice->getTotalRemainingStr(false) < $invoice->getTotalStr(false)) {
-  	        // downpayment was already done
-  	      }
-  	      else {
-    	      // calculate the downpayment
-    	      if ($invoice->downpayment == 'fixed') {
-    	        $total = $invoice->dpvalue;
+	    if ($invoice->id > 0) {
+  	    $hash = crypt($invoice->invoicenr, $invoice->invoicenr.'DEKAAG');
+  	    
+  	    if (isset($_GET['hash']) && $_GET['hash'] == $hash) {
+    	    $total = $invoice->getTotalRemainingStr(false);
+    	    $downpayment = false;
+    	    if (isset($_GET['downpayment']) && $invoice->downpayment != 'none') {
+    	      if ($invoice->getTotalRemainingStr(false) < $invoice->getTotalStr(false)) {
+    	        // downpayment was already done
     	      }
     	      else {
-    	        $total = ($invoice->getTotalStr(false)/100)*round($invoice->dpvalue);
+      	      // calculate the downpayment
+      	      if ($invoice->downpayment == 'fixed') {
+      	        $total = $invoice->dpvalue;
+      	      }
+      	      else {
+      	        $total = ($invoice->getTotalStr(false)/100)*round($invoice->dpvalue);
+      	      }
+      	      $downpayment = true;
     	      }
-    	      $downpayment = true;
-  	      }
-  	    }
-  	    
-  	    if ($total == 0) {
-  	      echo '<p><strong>'.__('This invoice was already paid','dekaagcrm').'</strong></p>';
+    	    }
+    	    
+    	    if ($total == 0) {
+    	      echo '<p><strong>'.__('This invoice was already paid','dekaagcrm').'</strong></p>';
+    	    }
+    	    else {
+      	    require_once dirname(__FILE__) . "/../lib/vendor/Mollie/API/Autoloader.php";
+      
+            $mollie = new Mollie_API_Client;
+            $mollie->setApiKey($instance['mollie_key']); 
+            
+            $protocol = isset($_SERVER['HTTPS']) && strcasecmp('off', $_SERVER['HTTPS']) !== 0 ? "https" : "http";
+          	$hostname = $_SERVER['HTTP_HOST'];
+          	
+          	if (isset($_SESSION['payment_id'])) {
+          	  $mollie_payment = $mollie->payments->get($_SESSION['payment_id']);
+            	if ($mollie_payment->status == 'paid') {
+            	  $payment = new DeKaagPayment;
+            	  $payment->total = $mollie_payment->amount;
+            	  $payment->paymethod = 'ideal';
+            	  $payment->status = 'success';
+            	  $payment->remarks = $mollie_payment->id.' '.$mollie_payment->paidDatetime;
+            	  $payment->{$payment->prefix().'invoice_id'} = $invoice->id;
+            	  $payment->date = date('Y-m-d H:i:s');
+            	  $payment->save();
+            	  
+            	  if($invoice->getTotalRemainingStr(false) == 0) {
+            	    $invoice->status = 4;
+            	  }
+            	  else {
+            	    $invoice->status = 3;
+            	  }
+            	  $invoice->save();
+            	  
+            	  echo '<p><strong>'.__('Your payment was received succesfully','dekaagcrm').'</strong></p>';
+            	}
+            	unset($_SESSION['payment_id']);
+          	}
+            else if ($_SERVER["REQUEST_METHOD"] != "POST")
+          	{
+          		$issuers = $mollie->issuers->all();
+              wp_enqueue_style('dekaagcrm-frontend', '/wp-content/plugins/dekaagcrm/css/frontend.css');
+              $what = $downpayment ? 'de aanbetaling' : ( $invoice->getTotalRemainingStr(false) < $invoice->getTotalStr(false) ? 'het resterende bedrag' : 'de factuur');
+              
+              echo '<p>Je staat op het punt om '.$what.' á <strong>€ '.number_format($total,2,',','.').'</strong> met iDeal af te rekenen.</p>';
+          		echo '<div id="widget-container"><form method="post">Selecteer je bank: <select name="issuer">';
+          
+          		foreach ($issuers as $issuer) {
+          			if ($issuer->method == Mollie_API_Object_Method::IDEAL)
+          			{
+          				echo "<option value=\"{$issuer->id}\">{$issuer->name}</option>";
+          			}
+          		}
+          		echo '<option value="">of selecteer later</option>';
+          		echo '</select> <button type="submit">Betaling uitvoeren</button></form></div>';
+          	}
+           	else {
+            	$order_id = $invoice->invoicenr;
+            	
+            	$payment = $mollie->payments->create(array(
+            		"amount"       => $total,
+            		"method"       => Mollie_API_Object_Method::IDEAL,
+            		"description"  => $downpayment ? __('Downpayment for', 'dekaagcrm').' '.$invoice->invoicenr : ($invoice->getTotalRemainingStr(false) < $invoice->getTotalStr(false) ? __('Remainder for', 'dekaagcrm').' '.$invoice->invoicenr : $invoice->invoicenr),
+            		"redirectUrl"  => "{$protocol}://{$hostname}/{$_SERVER['REQUEST_URI']}&order_id={$order_id}",
+            		"metadata"     => array(
+            			"order_id" => $order_id,
+            		),
+            		"issuer"       => !empty($_POST["issuer"]) ? $_POST["issuer"] : NULL
+            	));
+            	
+            	$_SESSION['payment_id'] = $payment->id;
+          
+              echo "<script type=\"text/javascript\">window.location.href='{$payment->getPaymentUrl()}';</script>";
+           	}
+    	    }
   	    }
   	    else {
-    	    require_once dirname(__FILE__) . "/../lib/vendor/Mollie/API/Autoloader.php";
-    
-          $mollie = new Mollie_API_Client;
-          $mollie->setApiKey($instance['mollie_key']); 
-          
-          $protocol = isset($_SERVER['HTTPS']) && strcasecmp('off', $_SERVER['HTTPS']) !== 0 ? "https" : "http";
-        	$hostname = $_SERVER['HTTP_HOST'];
-        	
-        	if (isset($_SESSION['payment_id'])) {
-        	  $mollie_payment = $mollie->payments->get($_SESSION['payment_id']);
-          	if ($mollie_payment->status == 'paid') {
-          	  $payment = new DeKaagPayment;
-          	  $payment->total = $mollie_payment->amount;
-          	  $payment->paymethod = 'ideal';
-          	  $payment->status = 'success';
-          	  $payment->remarks = $mollie_payment->id.' '.$mollie_payment->paidDatetime;
-          	  $payment->{$payment->prefix().'invoice_id'} = $invoice->id;
-          	  $payment->date = date('Y-m-d H:i:s');
-          	  $payment->save();
-          	  
-          	  if($invoice->getTotalRemainingStr(false) == 0) {
-          	    $invoice->status = 4;
-          	  }
-          	  else {
-          	    $invoice->status = 3;
-          	  }
-          	  $invoice->save();
-          	  
-          	  echo '<p><strong>'.__('Your payment was received succesfully','dekaagcrm').'</strong></p>';
-          	}
-          	unset($_SESSION['payment_id']);
-        	}
-          else if ($_SERVER["REQUEST_METHOD"] != "POST")
-        	{
-        		$issuers = $mollie->issuers->all();
-            wp_enqueue_style('dekaagcrm-frontend', '/wp-content/plugins/dekaagcrm/css/frontend.css');
-            $what = $downpayment ? 'de aanbetaling' : ( $invoice->getTotalRemainingStr(false) < $invoice->getTotalStr(false) ? 'het resterende bedrag' : 'de factuur');
-            
-            echo '<p>Je staat op het punt om '.$what.' á <strong>€ '.number_format($total,2,',','.').'</strong> met iDeal af te rekenen.</p>';
-        		echo '<div id="widget-container"><form method="post">Selecteer je bank: <select name="issuer">';
-        
-        		foreach ($issuers as $issuer) {
-        			if ($issuer->method == Mollie_API_Object_Method::IDEAL)
-        			{
-        				echo "<option value=\"{$issuer->id}\">{$issuer->name}</option>";
-        			}
-        		}
-        		echo '<option value="">of selecteer later</option>';
-        		echo '</select> <button type="submit">Betaling uitvoeren</button></form></div>';
-        	}
-         	else {
-          	$order_id = $invoice->invoicenr;
-          	
-          	$payment = $mollie->payments->create(array(
-          		"amount"       => $total,
-          		"method"       => Mollie_API_Object_Method::IDEAL,
-          		"description"  => $downpayment ? __('Downpayment for', 'dekaagcrm').' '.$invoice->invoicenr : ($invoice->getTotalRemainingStr(false) < $invoice->getTotalStr(false) ? __('Remainder for', 'dekaagcrm').' '.$invoice->invoicenr : $invoice->invoicenr),
-          		"redirectUrl"  => "{$protocol}://{$hostname}/{$_SERVER['REQUEST_URI']}&order_id={$order_id}",
-          		"metadata"     => array(
-          			"order_id" => $order_id,
-          		),
-          		"issuer"       => !empty($_POST["issuer"]) ? $_POST["issuer"] : NULL
-          	));
-          	
-          	$_SESSION['payment_id'] = $payment->id;
-        
-            echo "<script type=\"text/javascript\">window.location.href='{$payment->getPaymentUrl()}';</script>";
-         	}
+  	      echo '<p><strong>'.__('Illegal invoice link','dekaagcrm').'</strong></p>';
   	    }
-	    }
+	    } 
+	    else {
+	      echo '<p><strong>'.__('Unknown invoice','dekaagcrm').'</strong></p>';
+  	  }
 	  }
 	  echo $args['after_widget'];
 	}
