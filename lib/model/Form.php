da72043b76d8e6aa5238ce8d25bcd2ed66386283
Ricardo <ricardo@dev.mizar-it.nl>
2014-12-29 17:56:29 +0000

    Fixed problem with questions which have a default answer AND the default answer has a mutation.

--- lib/model/Form.php
+++ lib/model/Form.php
@@ -17,6 +17,29 @@ class DeKaagForm extends DeKaagBase {
     extract($vars);
     ob_start();
     $form = DeKaagForm::model()->findByPk($form_id);
+    
+    
+    $offset = 0;
+    
+    if ($form_id == 2 || $form_id == 4) {  
+      if(isset($_SESSION['booking']['answers'])) {
+        foreach ($_SESSION['booking']['answers'] as $key => $value) {
+          echo '<input type="hidden" name="answer_'.$key.'" id="answer_'.$key.'" value="'.$value.'">';
+          $row = DeKaagFormRow::model()->findByPk($key);
+          if ($row) {
+            if ($row->{$row->prefix().'form_id'} == 1 || $row->{$row->prefix().'form_id'} == 3) {
+              $mutations = json_decode($row->mutations);
+              $mutation = $mutations[$value];
+              $v = $mutation->type == 'price' ? $mutation->mutation : ($total / 100) * $mutation->mutation;
+              $total = $total + $v;
+            }
+          }
+        }
+        // TODO: foreach all visible form rows on the current form, which have a default answer that also has a mutation
+        // ie. vroegboekkorting is default yes, and this default answer has a mutation of -30 euro
+      }
+    }
+    
     if ($form) {
       $rows = DeKaagFormRow::model()->findAllByAttributes(new DeKaagCriteria(array($form->prefix().'form_id' => $form_id)));
       $mutationkeys = array();
@@ -30,11 +53,13 @@ class DeKaagForm extends DeKaagBase {
             
           }
           else {
+            $is_visible = true;
           ?>
       <div class="form-row" id="question_<?php echo $row->id; ?>" <?php if($row->visibleIf()) { 
         list($question_id, $answer_id) = $row->visibleIf();
-        if ($form->id == 1 || $_SESSION['booking']['answers'][$question_id] != $answer_id) {  
+        if ($form->id == 1 || $form->id == 3 || $_SESSION['booking']['answers'][$question_id] != $answer_id) {  
           echo ' style="display:none;"';
+          $is_visible = false;
         }
       } ?>>
         <h3><?php echo $row->title; ?></h3>
@@ -51,23 +76,33 @@ class DeKaagForm extends DeKaagBase {
             if (!is_null($row->default)) {
               if ($row->default == $key) {
                 $checked = ' checked="checked"';  
+                if ($is_visible && $mutation->mutation != 0) {
+                  $offset_m = ($mutation->type=='price' ? $mutation->mutation : ($total / 100) * $mutation->mutation);
+                  $offset += $offset_m;
+                  $mutationkeys[$row->id] = $offset_m;
+                }
               }
             }
             else {
               if ($first && $form->id == 1) {
                 $checked = ' checked="checked"';  
+                if ($is_visible && $mutation->mutation != 0) {
+                  $offset_m = ($mutation->type=='price' ? $mutation->mutation : ($total / 100) * $mutation->mutation);
+                  $offset += $offset_m;
+                  $mutationkeys[$row->id] = $offset_m;
+                }
                 $first = false;
               }
             }
             ?>
-            <input <?php echo $checked; ?> onclick="mutateTotal(<?php echo $row->id; ?>, '<?php echo $mutation->mutation; ?>', '<?php echo $mutation->type; ?>'<?php if ($form_id == 1) { ?>,'' , true<?php } ?>);" type="radio" name="answer_<?php echo $row->id; ?>" value="<?php echo $key; ?>" id="answer_<?php echo $row->id; ?>_<?php echo $key; ?>" style="width:auto;"> <label for="answer_<?php echo $row->id; ?>_<?php echo $key; ?>"><?php echo $answer; ?></label><br />
+            <input <?php echo $checked; ?> onclick="mutateTotal(<?php echo $row->id; ?>, '<?php echo $mutation->mutation!=''?$mutation->mutation:0; ?>', '<?php echo $mutation->type; ?>'<?php if ($form_id == 1) { ?>,'' , true<?php } ?>);" type="radio" name="answer_<?php echo $row->id; ?>" value="<?php echo $key; ?>" id="answer_<?php echo $row->id; ?>_<?php echo $key; ?>" style="width:auto;"> <label for="answer_<?php echo $row->id; ?>_<?php echo $key; ?>"><?php echo $answer; ?></label><br />
           <?php 
           }
           break;
           
         case 'select':
           ?>
-          <select style="width:450px;" onchange="mutateTotal(<?php echo $row->id; ?>, '<?php echo $mutation->mutation; ?>', '<?php echo $mutation->type; ?>'<?php if ($form_id == 1) { ?>,'', true<?php } ?>);" name="answer_<?php echo $row->id; ?>" id="answer_<?php echo $row->id; ?>_<?php echo $key; ?>" style="width:auto;">
+          <select style="width:450px;" onchange="mutateTotal(<?php echo $row->id; ?>, '<?php echo $mutation->mutation!=''?$mutation->mutation:0; ?>', '<?php echo $mutation->type; ?>'<?php if ($form_id == 1) { ?>,'', true<?php } ?>);" name="answer_<?php echo $row->id; ?>" id="answer_<?php echo $row->id; ?>_<?php echo $key; ?>" style="width:auto;">
           <?php
           foreach ($row->answers() as $key => $answer) { 
             $mutation = $mutations[$key];
@@ -75,11 +110,21 @@ class DeKaagForm extends DeKaagBase {
             if (!is_null($row->default)) {
               if ($row->default == $key) {
                 $checked = ' selected="selected"';  
+                if ($is_visible && $mutation->mutation != 0) {
+                  $offset_m = ($mutation->type=='price' ? $mutation->mutation : ($total / 100) * $mutation->mutation);
+                  $offset += $offset_m;
+                  $mutationkeys[$row->id] = $offset_m;
+                }
               }
             }
             else {
               if ($first && $form->id == 1) {
                 $checked = ' selected="selected"';  
+                if ($is_visible && $mutation->mutation != 0) {
+                  $offset_m = ($mutation->type=='price' ? $mutation->mutation : ($total / 100) * $mutation->mutation);
+                  $offset += $offset_m;
+                  $mutationkeys[$row->id] = $offset_m;
+                }
                 $first = false;
               }
             }
@@ -94,7 +139,7 @@ class DeKaagForm extends DeKaagBase {
           
         case 'input':
             $mutation = $mutations[0]; ?>
-            <input onkeyup="mutateTotal(this.value, '<?php echo $mutation->mutation; ?>', '<?php echo $mutation->type; ?>', <?php echo $row->id; ?><?php if ($form_id == 1) { ?>, true<?php } ?>);" type="text" name="answer_<?php echo $row->id; ?>" id="answer_<?php echo $row->id; ?>_<?php echo $key; ?>" style="width:450px;"><br />
+            <input onkeyup="mutateTotal(this.value, '<?php echo $mutation->mutation!=''?$mutation->mutation:0; ?>', '<?php echo $mutation->type; ?>', <?php echo $row->id; ?><?php if ($form_id == 1) { ?>, true<?php } ?>);" type="text" name="answer_<?php echo $row->id; ?>" id="answer_<?php echo $row->id; ?>_<?php echo $key; ?>" style="width:450px;"><br />
           <?php 
           break;
       }
@@ -177,33 +222,20 @@ window.mutateTotal = function(key, mutation, type, row, refresh)
 </script>
 <?php } ?>
 
-<?php if ($form_id == 2) {  
-  if(isset($_SESSION['booking']['answers'])) {
-    foreach ($_SESSION['booking']['answers'] as $key => $value) {
-      echo '<input type="hidden" name="answer_'.$key.'" id="answer_'.$key.'" value="'.$value.'">';
-      $row = DeKaagFormRow::model()->findByPk($key);
-      if ($row) {
-        if ($row->{$row->prefix().'form_id'} == 1) {
-          $mutations = json_decode($row->mutations);
-          $mutation = $mutations[$value];
-          $v = $mutation->type == 'price' ? $mutation->mutation : ($total / 100) * $mutation->mutation;
-          $total = $total + $v;
-        }
-      }
-    }
-  }
-  
+<?php if ($form_id == 2 || $form_id == 4) {  
   ?>
-
-<?php if ($form_id == 2) {  ?>
 <script>
 mutationkeys = <?php echo json_encode($mutationkeys); ?>;
 total = <?php echo $total; ?>;
+
+//console.log(<?php echo $total; ?>);
+//console.log(<?php echo $offset; ?>);
+//console.log(mutationkeys);
+//console.log(total);
 </script>
-<?php } ?>
 
 <div id="pricing-info">
-  <p>De totaalprijs wordt <strong>€ <span id="total-sum"><?php echo number_format($total,2,',','.'); ?></span></strong></p>
+  <p>De totaalprijs wordt <strong>€ <span id="total-sum"><?php echo number_format($total + $offset,2,',','.'); ?></span></strong></p>
 </div>      
 <?php } ?>
 <?php
