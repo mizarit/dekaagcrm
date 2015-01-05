ee46abc3778e285c918a485b7b378cb205feab2b
Ricardo <ricardo@dev.mizar-it.nl>
2015-01-05 16:55:55 +0000

    Changed login procedure to try locally first

--- lib/vendor/onlineafspraken/lib/widget.php
+++ lib/vendor/onlineafspraken/lib/widget.php
@@ -191,6 +191,29 @@ class Widget extends WidgetCore
 	 */
   public function executeLogin()
   {
+    // first, try locally
+  
+    $dekaag_user = DeKaagUser::model()->findByAttributes(new DeKaagCriteria(array('username' => $this->getRequestParameter('username'))));
+    if ($dekaag_user) {
+      // OA user does not exist, but De Kaag user does.
+      $salt = $dekaag_user->salt;
+      $password = crypt($this->getRequestParameter('password'), $salt);
+      if ($password == $dekaag_user->password) {
+        $_SESSION['dekaag_user_id'] = $dekaag_user->id;
+        $_SESSION['dekaag_relation_id'] = $dekaag_user->{$dekaag_user->prefix().'relation_id'};
+
+        $consumer = $this->sendRequest('loginCustomer', array(
+          'Username' => $this->getRequestParameter('username'),
+          'Password' => $this->getRequestParameter('password')
+        ));
+        if (isset($consumer['Customer'])) {
+          echo 'OK';
+          exit;
+        }
+      }
+    }	        
+
+    // then try externally
     $consumer = $this->sendRequest('loginCustomer', array(
       'Username' => $this->getRequestParameter('username'),
       'Password' => $this->getRequestParameter('password')
@@ -207,43 +230,6 @@ class Widget extends WidgetCore
       }
       else {
         // OA user exists, but no relation, create it
-       /*
-       array(17) {
-  ["Id"]=>
-  string(7) "1401981"
-  ["FirstName"]=>
-  string(7) "Ricardo"
-  ["LastName"]=>
-  string(7) "Matters"
-  ["Insertions"]=>
-  string(0) ""
-  ["BirthDate"]=>
-  string(0) ""
-  ["Gender"]=>
-  string(0) ""
-  ["Street"]=>
-  string(0) ""
-  ["HouseNr"]=>
-  string(0) ""
-  ["HouseNrAddition"]=>
-  string(0) ""
-  ["ZipCode"]=>
-  string(0) ""
-  ["City"]=>
-  string(0) ""
-  ["Country"]=>
-  string(9) "Nederland"
-  ["Phone"]=>
-  string(10) "0623231588"
-  ["MobilePhone"]=>
-  string(10) "0623231588"
-  ["Email"]=>
-  string(25) "ricardo.matters@gmail.com"
-  ["Status"]=>
-  string(1) "1"
-  ["OwnCompany"]=>
-  string(1) "1"
-  */
         $relation = new DeKaagRelation;
         $relation->title = $_SESSION['booking']['customer']['FirstName'].' '.$_SESSION['booking']['customer']['Insertions'].' '.$_SESSION['booking']['customer']['LastName'];
         $relation->first_name = $_SESSION['booking']['customer']['FirstName'];
